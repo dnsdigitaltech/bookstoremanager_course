d11d6cc56fe8fbedef4f362ce62945050b719a17
Davi Bernardo <dnssites@gmail.com>
2021-11-12 21:41:16 -0300

    First commit

---
+++ src/main/java/com/davibernardo/booksoremanager/BooksoremanagerApplication.java
@@ -0,0 +1,13 @@
+package com.davibernardo.booksoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksoremanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davibernardo/booksoremanager/BooksoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davibernardo.booksoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
